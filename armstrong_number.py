a84153c76e64b5d97e20f1da44a317d8873495c2
ShreyashSalian <wish4shreyu@gmail.com>
2019-09-27 18:29:53 +0530

    This program display the armstrong number with in a specified limit

---
+++ armstrong_number.py
@@ -0,0 +1,13 @@
+num = int(input("Enter The Number :"))
+sum = 0
+temp = num
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
+
+if num == sum:
+    print(num,"is a armstring number")
+
+else:
+    print(num,"is not armstrong number")
